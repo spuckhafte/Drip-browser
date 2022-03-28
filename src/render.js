0f9450c8df0a3768d63d182594f9a478c0365ee0
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-03-28 13:02:09 +0530

    ::first-commit::

---
+++ src/render.js
@@ -0,0 +1,5 @@
+$('#add-tab').click(element => {
+    let tab = $("<div class='tab nosel'></div>");
+    let tabHead = $("<div class='tab-head' id='cur-tab'><i class='fa fa-search'></i> New Tab</div>")
+    let tabTail = $("<span class='tab-tail' id='add-tab'><i class='fa fa-plus'></i></span>")
+})
